122a920c68c6dad80e4014b193cbd60d9df0578f
Julian Lipp <j.lipp@blumatix.at>
2019-10-31 15:06:29 +0100

    Updated test client for version 1.9

--- invoicefeaturedetection/src/com/blumatix/InvoiceDetailType.java
+++ invoicefeaturedetection/src/com/blumatix/InvoiceDetailType.java
@@ -6,22 +6,31 @@ package com.blumatix;
  */
 public enum InvoiceDetailType {
     None(0),
+    Sender(2),
     DeliveryDate(8),
     GrandTotalAmount(16),
+    VatRate(32),
     InvoiceDate(64),
+    Receiver(128),
+    NetTotalAmount(256),
     InvoiceId(1024),
     DocumentType(8192),
-    VatAmount(131072),
     Iban(16384),
+    Bic(32768),
+    LineItem(65536),
+    VatAmount(131072),
     InvoiceCurrency(524288),
+    DeliveryNoteId(1048576),
     CustomerId(2097152),
+    TaxNumber(4194304),
     UId(8388608),
     SenderOrderId(16777216),
     ReceiverOrderId(33554432),
     SenderOrderDate(67108864),
     ReceiverOrderDate(134217728),
+    NetAmount(268435456),
     VatGroup(536870912),
-    CustomInvoiceDetail(-2147483648);
+    VatTotalAmount(1073741824);
 
     private final int id;
 
